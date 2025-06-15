ede1c1698df0a04d62f4cf9d8faac724828b0634
lakshmeesh12 <lakshmeeshhnluckey777@gmail.com>
2025-06-15 16:59:58 +0530

    Add files via upload

--- Frontend_dev/dev/src/App.tsx
+++ Frontend_dev/dev/src/App.tsx
@@ -1,30 +1,30 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
-import { useApp } from "./contexts/AppContext";
+import { useApp } from "./contexts/AppContext"; // Keep this for NewTicketNotification, if you decide to re-enable it later
 import { MainLayout } from "./components/layout/MainLayout";
 import Index from "./pages/Index";
 import TicketsPage from "./pages/TicketsPage";
 import TicketDetailPage from "./pages/TicketDetailPage";
 import ChatPage from "./pages/ChatPage";
 import SettingsPage from "./pages/SettingsPage";
-import { RequestsTracker } from "./pages/RequestsTracker"; // New import
+import { RequestsTracker } from "./pages/RequestsTracker";
 import NotFound from "./pages/NotFound";
-import NewTicketNotification from "./components/notifications/NewTicketNotification";
+import NewTicketNotification from "./components/notifications/NewTicketNotification"; // Keep this for NewTicketNotification, if you decide to re-enable it later
 
 const queryClient = new QueryClient();
 
 function AppRoutes() {
+  // If you also want to remove the specific NewTicketNotification component,
+  // then remove these lines and the JSX block below.
   const { showNewTicketNotification, setShowNewTicketNotification, currentNewTicket } = useApp();
 
   return (
     <>
+      {/* If you want to keep NewTicketNotification but remove toasts, keep this block */}
       {showNewTicketNotification && currentNewTicket && (
-        <NewTicketNotification 
+        <NewTicketNotification
           onClose={() => setShowNewTicketNotification(false)}
           ticket={currentNewTicket}
         />
@@ -48,8 +48,9 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AppProvider>
-        <Toaster />
-        <Sonner position="top-right" />
+        {/* REMOVE these lines to remove the toaster */}
+        {/* <Toaster /> */}
+        {/* <Sonner position="top-right" /> */}
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
